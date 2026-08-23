14eda5d0473af3ee034ff55a22bb902c4a2a4387
Omer <omersabribalci@gmail.com>
2026-08-23 17:08:21 -0400

    Squashed '17-SQL-Join-Aggregation/' content from commit 346d64f
    git-subtree-dir: 17-SQL-Join-Aggregation
    git-subtree-split: 346d64f375af4282550e13f09c9b8832ac501e04

---
+++ src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
@@ -0,0 +1,26 @@
+package com.workintech.sqldmljoins.repository;
+
+import com.workintech.sqldmljoins.entity.Kitap;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface KitapRepository extends JpaRepository<Kitap, Long> {
+
+    //Dram ve Hikaye türündeki kitapları listeleyin. JOIN kullanmadan yapın.
+    String QUESTION_1 = "SELECT * FROM kitap\n" +
+            "WHERE turno IN (\n" +
+            "\tSELECT turno FROM tur\n" +
+            "\tWHERE ad = 'Dram' OR ad = 'Hikaye'\n" +
+            ")";
+    @Query(value = QUESTION_1, nativeQuery = true)
+    List<Kitap> findBooks();
+
+
+    String QUESTION_10 = "SELECT AVG(puan) from kitap";
+    @Query(value = QUESTION_10, nativeQuery = true)
+    Double findAvgPointOfBooks();
+
+
+}
